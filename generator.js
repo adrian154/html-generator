836621eff38a0d7128137587881b447b29747ad5
adrian <nyancatgaming9999@gmail.com>
2021-12-25 21:40:40 -0800

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,4 +1,4 @@
-// generate all the tag-generating functions statically so that 
+// generate all the tag-generating functions statically to play nice with IDEs
 const regularElements = ["a","abbr","address","article","aside","audio","b","bdi","bdo","blockquote","body","button","canvas","caption","cite","code","colgroup","data","datalist","dd","del","details","dfn","dialog","div","dl","dt","em","fieldset","figcaption","figure","footer","form","h1","h2","h3","h4","h5","h6","head","header","html","i","iframe","ins","kbd","label","legend","li","main","map","mark","math","menu","meter","nav","noscript","object","ol","optgroup","option","output","p","picture","portal","pre","progress","q","rp","rt","ruby","s","samp","script","section","select","slot","small","span","strong","style","sub","summary","sup","svg","table","tbody","td","template","textarea","tfoot","th","thead","time","title","tr","u","ul","var","video"];
 const voidElements = ["area", "base", "br", "col", "embed", "hr", "img", "input", "link", "meta", "param", "source", "track", "wbr"];
 
