146780813eed64d229d8966980a1f090a03e665f
adrian154 <94568a@gmail.com>
2022-02-01 20:07:38 -0800

    simplify

--- template.js
+++ template.js
@@ -31,8 +31,7 @@ const regularElement = (tag, content) => {
 const voidElement = (tag, attributes) => ({html: openingTag(tag, attributes)});
 
 // helper functions
-module.exports["regularElement"] = regularElement;
-module.exports["voidElement"] = voidElement;
+module.exports["tag"] = regularElement;
 module.exports["raw"] = str => ({html: str});
 
 /* --- code below this point is statically generated --- */
